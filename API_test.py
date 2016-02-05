98e890c073381afbfd64f0395e344a4b13893aac
Tom Miller at ParkWhiz <TomAtParkwhiz@Toms-MacBook-Pro.local>
2016-02-05 17:08:27 -0600

    Second Commit

--- API_test.py
+++ API_test.py
@@ -1,6 +1,6 @@
 import requests
 response = requests.get(
-    "https://www.eventbriteapi.com/v3/events/search/?&venue.city=Chicago&start_date.range_start=2016-09-15T00:00:00Z",
+    "https://www.eventbriteapi.com/v3/events/search/?&venue.city=New_York&start_date.range_start=2016-13-15T00:00:00Z",
     headers = {
         "Authorization": "Bearer ORQZ3DG2UZYWRAX77NJW",
     },
