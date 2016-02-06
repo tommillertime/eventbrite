e697fcad04ada7f6facd48510738333575d94694
Tom Miller <thomasjlmiller@gmail.com>
2016-02-05 22:26:41 -0600

    Remove key

--- API_test.py
+++ API_test.py
@@ -2,7 +2,7 @@ import requests
 response = requests.get(
     "https://www.eventbriteapi.com/v3/events/search/?&venue.city=New_York&start_date.range_start=2016-13-15T00:00:00Z",
     headers = {
-        "Authorization": "Bearer ORQZ3DG2UZYWRAX77NJW",
+        "Authorization": "Bearer ",
     },
 )
 print response.json()['events'][0]['name']['text']
