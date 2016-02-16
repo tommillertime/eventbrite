20042ff64fd732147b440b5765296c12eac04068
Tom Miller <thomasjlmiller@gmail.com>
2016-02-15 22:53:46 -0600

    Update API_test.py

--- API_test.py
+++ API_test.py
@@ -1,9 +1,27 @@
 import requests
+import pprint
+import csv
+
 response = requests.get(
-    "https://www.eventbriteapi.com/v3/events/search/?&venue.city=New_York&start_date.range_start=2016-13-15T00:00:00Z",
+    "https://www.eventbriteapi.com/v3/events/search/?&venue.city=New_York&start_date.range_start=2016-02-22T00:00:00Z&categories=111&expand=venue,category",
     headers = {
-        "Authorization": "Bearer ",
+        "Authorization": "Bearer ORQZ3DG2UZYWRAX77NJW",
     },
 )
-print response.json()['events'][0]['name']['text']
+#pprint.pprint(response.json()['events'])
+x = 2
 
+for i in range(0,5):
+    name = response.json()['events'][i]['name']['text']
+    url = response.json()['events'][i]['url']
+    venue_long = response.json()['events'][i]['venue']['address']['longitude']
+    ven_long = "{:.9f}".format(venue_long)
+    venue_lat = response.json()['events'][i]['venue']['address']['latitude']
+    start_date = response.json()['events'][i]['start']['local']
+
+
+    with open('eventbrite.csv', 'wb') as csvfile:
+        fieldnames = ['name', 'url', 'venue_long', 'venue_lat', 'start_date']
+        spamwriter = csv.DictWriter(csvfile, fieldnames = fieldnames)
+        spamwriter.writeheader()
+        spamwriter.writerow({'name': name, 'url' : url, 'venue_long': venue_long, 'venue_lat': venue_lat, 'start_date': start_date})
